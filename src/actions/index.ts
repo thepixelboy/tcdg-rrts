c10bfd4bc69df49001d271c4fcf74194fd84dfc4
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-03-04 15:21:24 +0100

    Generic dispatch function

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -9,13 +9,18 @@ interface Todo {
   completed: boolean;
 }
 
+interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
+}
+
 const url = 'http://jsonplaceholder.typicode.com/todos';
 
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
     const response = await axios.get<Todo[]>(url);
 
-    dispatch({
+    dispatch<FetchTodosAction>({
       type: ActionTypes.fetchTodos,
       payload: response.data
     });
